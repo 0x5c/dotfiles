9ca20de9c00e46a056b87ce188610328167a2b5d
0x5c <dev@0x5c.io>
2021-08-12 16:11:04 -0400

    Added proper prompt formatting for git rebase

--- exact_dot_zsh/19prompt.zsh
+++ exact_dot_zsh/19prompt.zsh
@@ -59,16 +59,16 @@ zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' unstagedstr '*'
 zstyle ':vcs_info:git:*' stagedstr '+'
 zstyle ':vcs_info:git:*' formats '%F{12}g:%b%m%u%c%f '
-zstyle ':vcs_info:git:*' actionformats '%F{12}g:[%F{9}%a%F{12}]%b%m%u%c%f '
+zstyle ':vcs_info:git:*' actionformats '%F{12}g:[%F{9}%a%F{12}]%b%m%F{12}%u%c%f '
+zstyle ':vcs_info:git:*' patch-format '%F{3}@%F{10}%1.10p%F{3}(%F{6}%n%F{3}/%F{6}%a%F{3})'
+zstyle ':vcs_info:git:*' nopatch-format '%F{3}(%F{6}%n%F{3}/%F{6}%a%F{3})'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
 +vi-git-untracked() {
     if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
        git status --porcelain | grep -m 1 '^??' &>/dev/null
     then
-        hook_com[misc]='?'
-    else
-        hook_com[misc]=''
+        hook_com[misc]=${hook_com[misc]}'%F{12}?'
     fi
 }
 
