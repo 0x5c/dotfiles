9f9b60f0d33789b229f94ffbf06ec79d44af7fca
0x5c <dev@0x5c.io>
2021-09-07 04:17:50 -0400

    Added title setting for the command being run

--- exact_dot_zsh/19title.zsh
+++ exact_dot_zsh/19title.zsh
@@ -1,9 +1,22 @@
+# Custom terminal window title handling
+
+
 # Sets the terminal title to
-# user@host pwd (pty)
-
-
+#   user@host: pwd (pty)
+# when at the prompt
 function precmd_term_title() {
     print -nP -u2 "\033]0;%n@%m: %3~ (%l)\007"
 }
 
 precmd_functions+=( precmd_term_title )
+
+
+# Sets the terminal title to
+#   user@host$ command (pty)
+# when running a command
+function preexec_term_title() {
+    input=(${=2})
+    print -nP -u2 "\033]0;%n@%m\$ ${input[1]} (%l)\007"
+}
+
+preexec_functions+=( preexec_term_title )
