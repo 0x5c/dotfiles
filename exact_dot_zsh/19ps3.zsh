3f9659c48f46b5ee8c808c9706da52a08db00a41
0x5c <dev@0x5c.io>
2022-03-04 03:08:36 -0500

    zsh: added PS3 and PS4 prompts

---
+++ exact_dot_zsh/19ps3.zsh
@@ -0,0 +1,7 @@
+# A colourful PS3 prompt
+# 2022 - 0x5c
+
+# Question: Teal
+# Symbol: Bright Magenta
+
+PS3='%F{6}Choice? %F{13}»%f '
---
+++ exact_dot_zsh/19ps4.zsh
@@ -0,0 +1,11 @@
+# A colourful PS4 prompt
+# 2022 - 0x5c
+
+# Starting symbol: Bright Black
+# Shell construct depth: Bright Red
+# Shell construct name: Teal
+# Separator: Bright Black
+# Current line: Yellow
+# End symbol: Briht Black
+
+PS4='%F{8}+ %F{9}%e %F{6}%N%F{8}:%F{3}%i%F{8}> %f'
