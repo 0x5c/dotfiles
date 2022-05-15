dac9729a2a88c6dd5b1982c67feb9452e2f3cc29
0x5c <dev@0x5c.io>
2022-05-15 04:35:42 -0400

    micro: use xlint for srcpkg templates

--- dot_config/exact_micro/plug/srcpkg-lint/srcpkglint.lua
+++ dot_config/exact_micro/plug/srcpkg-lint/srcpkglint.lua
@@ -1,5 +1,6 @@
 VERSION = "1.0.0"
 
 function preinit()
-    linter.makeLinter("pkg-shellcheck", "srcpkg", "shellcheck", {"-f", "gcc", "-s", "bash", "%f"}, "%f:%l:%c:.+: %m")
+    --linter.makeLinter("pkg-shellcheck", "srcpkg", "shellcheck", {"-f", "gcc", "-s", "bash", "%f"}, "%f:%l:%c:.+: %m")
+    linter.makeLinter("xlint", "srcpkg", "xlint", {"%f"}, "%f:%l: %m")
 end
