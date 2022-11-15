bab1afdac9beaf2efc36d81ae656c63cd9bc7278
0x5c <dev@0x5c.io>
2022-11-14 22:08:41 -0500

    zsh: switch to fast-syntax-highlighting
    Keep zsh-syntax-highlighting as fallback, if the system provides it.

--- exact_dot_zsh/80syntaxhighlight.zsh
+++ exact_dot_zsh/80syntaxhighlight.zsh
@@ -1,1 +1,13 @@
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh &>/dev/null || true
+# commandline syntax highlighting
+
+fshpath=$HOME/.local/share/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+slow_sh_path=/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+if [ -f "$fshpath" ]; then
+    source $fshpath || true
+    return
+fi
+
+if [ -f "$slow_sh_path" ]; then
+    source $slow_sh_path &>/dev/null || true
+fi
---
+++ utils/executable_install-fsh.zsh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $HOME/.local/share/fast-syntax-highlighting
